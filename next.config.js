fd1ed4e1ab7d5ffb16d9317a6f8dfd8c78feb364
Muhammad <kholzoda2001@gmail.com>
2026-05-03 03:02:17 +0400

    fix: use top-level serverExternalPackages for pdfkit (correct Next.js config key)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['pdfkit'],
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'pdfkit'],
   },
